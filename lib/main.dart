3c649dcaba6b5434219dde01764eeec35f9493b2
Ady Botas <adiymb@gmail.com>
2021-11-28 21:56:41 +0200

    primul commit pentru fluttre

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const title = Text("Aplicatie test");
+  static const appBarTheme = AppBarTheme(
+    color: Colors.purple,
+  );
+  static const List<Widget> butoaneBottomBar = [Icon(Icons.add)];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple[900],
+        appBarTheme: appBarTheme,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: title,
+        ),
+        body: const Center(
+          child: Text("Ce aplicatie faina"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Acasa",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.work),
+              label: "Lucru",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Setari",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
